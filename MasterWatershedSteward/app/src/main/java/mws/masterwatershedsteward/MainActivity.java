a33b43f7ed0ea3d4247e5dc0cc87caa9cae19e72
asamin <asamin@comcast.net>
2016-07-21 17:30:35 -0400

    :D

--- MasterWatershedSteward/app/src/main/java/mws/masterwatershedsteward/MainActivity.java
+++ MasterWatershedSteward/app/src/main/java/mws/masterwatershedsteward/MainActivity.java
@@ -2,12 +2,17 @@ package mws.masterwatershedsteward;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        buttTest = (Button) findViewById(R.id.buttTest);
+        buttTest.setOnClickListener(
+                System.out.println("Tits");
+        );
         setContentView(R.layout.activity_main);
     }
 }
